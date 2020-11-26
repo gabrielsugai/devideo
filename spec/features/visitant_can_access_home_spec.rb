dc720c06223a5788c852fa02b1905eae2513c3da
Gabriel Sugai <gabrielsugai@gmail.com>
2020-11-26 20:21:59 -0300

    Visitant can view all video details

--- spec/features/visitant_can_access_home_spec.rb
+++ spec/features/visitant_can_access_home_spec.rb
@@ -11,4 +11,18 @@ feature 'Visitant can access home page' do
     expect(page).to have_content(videoA.title)
     expect(page).to have_content(videoB.title)
   end
+
+  scenario 'and can view a video details' do
+    videoA = Video.create!(title: 'Video 1', description: 'Video1')
+    videoB = Video.create!(title: 'Video 2', description: 'Video2')
+
+    visit root_path
+    click_on videoA.title
+
+    expect(page).to have_content(videoA.title)
+    expect(page).to have_content(videoA.description)
+    expect(page).not_to have_content(videoB.title)
+    expect(page).not_to have_content(videoB.description)
+
+  end
 end
